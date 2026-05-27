a1c3369402d4a3741d6fcc644ee8286bc4ade8a5
Mateus Vinicius Figueredo de Araújo <mateus.vf.araujo003@gmail.com>
2026-05-27 14:18:25 -0300

    Atualização do Banco de Dados e inserção dos dados publicos

--- database/database.py
+++ database/database.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-# Ssubstitua usuario, senha, host, porta e nome do banco pelos dados locais
+# Antes de rodar esse script, é necessário que o MySQL esteja rodando e que o banco safer_db exista. 
+# Pode ser criado usando um cliente MySQL ou via linha de comando com:
+# mysql -u 'usuario' -p -e "CREATE DATABASE safer_db;"
+
+# Substitua usuario, senha, host, porta e nome do banco pelos dados locais
 DATABASE_URL = "mysql+pymysql://usuario:senha@localhost:3306/safer_db"
 
 # Cria o motor de conexao
@@ -13,7 +17,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def init_db():
-    # Esse comando cria no MySQL as tabelas mapeadas pelas classes que herdam de Base
+    # Esse comando cria no MySQL as tabelas mapeadas pelas classes que herdam da Base
     Base.metadata.create_all(bind=engine)
     print("Tabelas criadas com sucesso!")
 
